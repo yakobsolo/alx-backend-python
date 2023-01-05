f399f867f350ee1d062e3c9834193345775624e5
yakobsolo <yakisolo2009@gmail.com>
2023-01-05 12:19:50 +0000

    return type tuple str, float

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ basic annotation - complex types """
-from typing import Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ return tuple type str float """
     return (k, v**2)
